07d1eeffe28f7db52338c7dff331d1175586f540
NideeshN <112001028@smail.iitpkd.ac.in>
2023-11-26 23:54:21 +0530

    moved button click function to separate js file

--- renderer.php
+++ renderer.php
@@ -249,38 +249,7 @@ class qtype_essaynew_renderer extends qtype_renderer {
             'aria-labelledby' => $labelbyid,
             'class' => 'list-unstyled m-0',
         ]);
-        $output .= '<script>
-                function annotate(attemptid, slot, fileno) {
-                    var formData = new FormData();
-                    formData.append("attempt", attemptid);
-                    formData.append("slot", slot);
-                    formData.append("fileno", fileno);
-                
-                    // Create a new form element dynamically
-                    var form = document.createElement("form");
-                    form.method = "post";
-                    form.action = "../../question/type/essaynew/annotator/annotator.php";
-                
-                    // Append form data as hidden input fields
-                    for (var pair of formData.entries()) {
-                        var input = document.createElement("input");
-                        input.type = "hidden";
-                        input.name = pair[0];
-                        input.value = pair[1];
-                        form.appendChild(input);
-                    }
-                
-                    // Append the form to the document body
-                    document.body.appendChild(form);
-                
-                    // Submit the form
-                    form.submit();
-                
-                    // Remove the form from the document body after submission
-                    form.remove();
-                }
-                
-            </script>';    
+        $output .= '<script type="text/javascript" src="/question/type/essaynew/js/annotatebutton.js"></script>';    
         return $output;
     }
 
