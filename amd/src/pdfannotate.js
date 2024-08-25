ea2220ba0bed27bdecce0a48130ace839f03cd24
vmsreeram <112001051@smail.iitpkd.ac.in>
2024-08-25 21:41:23 +0530

    changed license to MIT. ref #30

--- amd/src/pdfannotate.js
+++ amd/src/pdfannotate.js
@@ -1,18 +1,3 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * OnClick handlers are defined here
  * These functions will call functions in pdfannotate.js.
@@ -20,7 +5,7 @@
  * @module     qtype_essayannotate/pdfannotate
  * @copyright  2024 IIT Palakkad
  * @copyright  based on work done by Ravisha Hesh {@link https://github.com/RavishaHesh/PDFJsAnnotations}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    http://opensource.org/licenses/mit-license The MIT License
  * PDFAnnotate v1.0.1
  * @author     Ravisha Hesh
  * @updatedby  Asha Jose and Parvathy S Kumar
