3f99b5beb5c3ea9c68d0b22b6c36cb8f88662f06
vmsreeram <112001051@smail.iitpkd.ac.in>
2024-11-09 21:14:10 +0530

    renamed annotatormustacheconfig to fix #33

--- annotator/annotator.php
+++ annotator/annotator.php
@@ -40,7 +40,7 @@
  */
 
 require_once('../../../../config.php');
-require_once('annotatorMustacheConfig.php');
+require_once('annotatormustacheconfig.php');
 require_once('../../../../mod/quiz/locallib.php');
 require_login();
 use qtype_essayannotate\helper;
